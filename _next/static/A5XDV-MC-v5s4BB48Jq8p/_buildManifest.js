ca93bdfdcfb33edd37eb4bc7dc30187ad0069439
Damla Başak Koçak <kocakdamlabasak@gmail.com>
2026-07-21 10:35:57 +0300

    Updates

---
+++ _next/static/A5XDV-MC-v5s4BB48Jq8p/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/mk-partners//_next/:path+",
+        "destination": "/mk-partners/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
